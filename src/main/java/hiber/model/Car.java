9f5d33e0f069639b5bacc8f6cc999c281652907a
ITProgramisttechnar <samyro001@mail.ru>
2026-02-04 21:04:21 +0300

    добавил lombok

--- src/main/java/hiber/model/Car.java
+++ src/main/java/hiber/model/Car.java
@@ -1,9 +1,13 @@
 package hiber.model;
 
-import org.hibernate.procedure.spi.ParameterRegistrationImplementor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+@NoArgsConstructor
 
+@Data
 @Entity
 @Table(name = "cars")
 public class Car {
@@ -18,32 +22,10 @@ public class Car {
     @OneToOne(mappedBy = "car", fetch = FetchType.EAGER)
     private User user;
 
-    public Car(){
-
-    }
     public Car(String model,int series){
         this.model = model;
         this.series = series;
     }
-    public int getSeries(){
-        return series;
-    }
-    public String getModel(){
-        return model;
-    }
-    public Long getId(){
-        return id;
-    }
-    public User getUser(){return user;}
-    public void setUser(User user){this.user=user;}
-    public void setId(Long id){this.id=id;}
-    public void setSeries(int series){
-        this.series = series;
-    }
-    public void setModel(String model){
-        this.model=model;
-    }
-
     @Override
     public String toString(){
         return String.format(
